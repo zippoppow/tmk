29d186308e3fbf4aa07d92290ce78660d696f14c
zippoppow <zippoppow@yahoo.com>
2026-04-14 00:15:10 -0700

    enabling external workspace imports

--- example-app/next.config.js
+++ example-app/next.config.js
@@ -1,14 +1,29 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const RAILWAY_API = process.env.NEXT_PUBLIC_TMK_API_URL_PRODUCTION || 'https://tmk-api.up.railway.app';
 
 const nextConfig = {
+  // We import shared files from ../theme, ../tokens, and ../icons.
+  // Allow transpiling and resolving external workspace files.
+  experimental: {
+    externalDir: true,
+  },
+
   // Path alias to import Pegasus library components locally
   webpack: (config) => {
+    const appNodeModules = path.resolve(__dirname, 'node_modules');
+
     config.resolve.alias = {
       ...config.resolve.alias,
-      // Allows importing from '../tokens', '../theme', '../icons'
-      // In a real monorepo, you'd use workspaces or npm link
+      '@mui/material': path.resolve(appNodeModules, '@mui/material'),
+      '@mui/icons-material': path.resolve(appNodeModules, '@mui/icons-material'),
+      '@emotion/react': path.resolve(appNodeModules, '@emotion/react'),
+      '@emotion/styled': path.resolve(appNodeModules, '@emotion/styled'),
     };
+
+    config.resolve.modules = [appNodeModules, ...(config.resolve.modules || [])];
+
     return config;
   },
 
